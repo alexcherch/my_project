12bc9ac276db4cba721402aa4f5fc6d4130caefb
Alexander Potashev <alexcherch0@gmail.com>
2026-03-27 15:04:10 +0300

    новая функция

--- src/widget.py
+++ src/widget.py
@@ -3,7 +3,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(card_or_account_number: str) -> str:
     """Функция, которая принимает строку, содержащую тип и номер карты или счета,
-       и возвращает строку с замаскированным номером"""
+    и возвращает строку с замаскированным номером"""
     splits = card_or_account_number.split()
     new_splits = []
     for split in splits:
@@ -15,3 +15,8 @@ def mask_account_card(card_or_account_number: str) -> str:
         else:
             new_splits.append(split)
     return " ".join(new_splits)
+
+
+def get_date(date: str) -> str:
+    """Функция, которая принимает на вход строку с полной датой и возвращает строку с датой в формате 'ДД.ММ.ГГГГ'"""
+    return date[8:10] + "." + date[5:7] + "." + date[:4]
