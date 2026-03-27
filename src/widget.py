c6f0a5bde4752fe7341fad42a2260db8c17f3646
Alexander Potashev <alexcherch0@gmail.com>
2026-03-27 14:02:02 +0300

    add docstring

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,8 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_or_account_number: str) -> str:
+    """Функция, которая принимает строку, содержащую тип и номер карты или счета,
+       и возвращает строку с замаскированным номером"""
     splits = card_or_account_number.split()
     new_splits = []
     for split in splits:
