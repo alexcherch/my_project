0783908d69185f73a7532f92e2176c765fdb05c4
Alexander Potashev <alexcherch0@gmail.com>
2026-04-12 17:40:28 +0300

    correct processing.py

--- src/processing.py
+++ src/processing.py
@@ -9,8 +9,8 @@ def filter_by_state(list_of_states: list[dict], state: str = "EXECUTED") -> list
     return filtered_list
 
 
-def sort_by_date(list_of_dates: list[dict], reverse: bool = True) -> list[dict]:
+def sort_by_date(list_of_dates: list[dict], ascending: bool = False) -> list[dict]:
     """Сортирует список словарей по дате."""
-    sorted_list = sorted(list_of_dates, key=lambda item: item["date"], reverse=reverse)
+    sorted_list = sorted(list_of_dates, key=lambda item: item["date"], reverse=ascending)
 
     return sorted_list
